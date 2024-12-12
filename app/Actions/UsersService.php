36ec1042732f56705639d78cc63b1e1313244b53
biologed <48013636+biologed@users.noreply.github.com>
2024-12-12 16:11:29 +0300

    create users service

---
+++ app/Actions/UsersService.php
@@ -0,0 +1,111 @@
+<?php
+
+namespace App\Actions;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
+use Illuminate\Support\MessageBag;
+use Illuminate\Validation\Rules\Password;
+use Illuminate\Validation\ValidationException;
+
+class UsersService
+{
+    private const int ITEM_PER_PAGE = 5;
+    /**
+     * @throws ValidationException
+     */
+    public function showAll(Request $request): MessageBag|User
+    {
+        $request->merge(['order', strtolower($request->get('order'))]);
+        $validator = Validator::make($request->all(), [
+            'page' => ['int'],
+            'name' => ['string', 'present_with:order', 'max:255'],
+            'order' => ['string', 'lowercase', 'in:desc,asc'],
+        ]);
+        if ($validator->passes()) {
+            $validated = $validator->validated();
+            $usersList = User::orderBy('name', $validated['order'] ?? null);
+            if (isset($validated['name'])) {
+                $usersList = $usersList->where('name', 'LIKE', '%' . $validated["name"] . '%');
+            }
+            return $usersList->simplePaginate(self::ITEM_PER_PAGE, ['*'], 'page', $validated['page'] ?? 1);
+        }
+        return $validator->messages();
+    }
+
+    /**
+     * @throws ValidationException
+     */
+    public function showOne(string|int $id, Request $request): MessageBag|User
+    {
+        $request->merge(['id' => $id]);
+        $validator = Validator::make($request->all(), [
+            'id' => ['required', 'int', "exists:users,id,id,$request->id"],
+        ]);
+        if ($validator->passes()) {
+            $validated = $validator->validated();
+            return User::find($validated['id']);
+        }
+        return $validator->messages();
+    }
+
+    /**
+     * @throws ValidationException
+     */
+    public function save(Request $request, string|int $id = null): MessageBag|User
+    {
+        if ($id > 0) {
+            //Для корректного обновления
+            $request->merge(['id' => $id, 'save_method' => 'update']);
+        }
+        $validator = Validator::make($request->all(), [
+            'id' => ['required_if:save_method,update', 'int', "exists:users,id,id,$request->id"],
+            'name' => ['required_if:save_method,null', 'string', 'max:255'],
+            'ip' => ['required_if:save_method,null', 'ip', 'max:255'],
+            'comment' => ['required_if:save_method,null', 'string', 'max:255'],
+            'email' => ['required_if:save_method,null', 'email', 'max:255', 'unique:users'],
+            'password' => ['required_if:save_method,null', 'string', Password::default()],
+        ]);
+
+        //Создаем пользователя только из валидных полей
+        if ($validator->passes()) {
+            $validated = $validator->validated();
+            if ($request->save_method === 'update') {
+                $user = User::find($validated['id']);
+                $user->name = $validated['name'] ?? $user->name;
+                $user->ip = $validated['ip'] ?? $user->ip;
+                $user->comment = $validated['comment'] ?? $user->comment;
+                $user->password = $validated['password'] ?? $user->password;
+                $user->save();
+                return $user;
+            }
+            return User::create([
+                'name' => $validated['name'],
+                'ip' => $validated['ip'],
+                'comment' => $validated['comment'],
+                'email' => $validated['email'],
+                'password' => $validated['password'],
+            ]);
+        }
+
+        return $validator->messages();
+    }
+
+    /**
+     * @throws ValidationException
+     */
+    public function delete(string|int $id, Request $request): MessageBag
+    {
+        $request->merge(['id' => $id]);
+        $validator = Validator::make($request->all(), [
+            'id' => ['required', 'int', "exists:users,id,id,$request->id"],
+        ]);
+        if ($validator->passes()) {
+            $user = User::find($id);
+            $user->delete();
+        }
+
+        return $validator->messages();
+    }
+}
--- app/Http/ApiControllers/UsersController.php
+++ app/Http/ApiControllers/UsersController.php
@@ -2,16 +2,18 @@
 
 namespace App\Http\ApiControllers;
 
-use App\Models\User;
+use App\Actions\UsersService;
 use Illuminate\Http\Request;
 use Illuminate\Http\Response;
-use Illuminate\Support\Facades\Validator;
-use Illuminate\Validation\Rules\Password;
 use Illuminate\Validation\ValidationException;
 
 class UsersController extends Controller
 {
-    private const int ITEM_PER_PAGE = 5;
+    public function __construct(
+        public UsersService $usersService
+    ) {
+        parent::__construct();
+    }
 
     /**
      * Display a listing of the resource.
@@ -23,26 +25,7 @@ class UsersController extends Controller
      */
     public function index(Request $request): Response
     {
-        $request->merge(['order', strtolower($request->get('order'))]);
-        $validator = Validator::make($request->all(), [
-            'page' => ['int'],
-            'name' => ['string', 'present_with:order', 'max:255'],
-            'order' => ['string', 'lowercase', 'in:desc,asc'],
-        ]);
-        if ($validator->passes()) {
-            $validated = $validator->validated();
-            $usersList = User::select(['id','name']);
-            if (isset($validated['name'])) {
-                $usersList = $usersList->where('name', 'LIKE', '%' . $validated["name"] . '%');
-            }
-            if (isset($validated['order'])) {
-                $usersList = $usersList->orderBy('name', $validated['order']);
-            }
-            return response(
-                $usersList->simplePaginate(self::ITEM_PER_PAGE, ['*'], 'page', $validated['page'] ?? 1)
-            );
-        }
-        return response($validator->messages());
+        return response($this->usersService->showAll($request));
     }
 
     /**
@@ -55,25 +38,7 @@ class UsersController extends Controller
      */
     public function store(Request $request): Response
     {
-        $validator = Validator::make($request->all(), [
-            'name' => ['required', 'string', 'max:255'],
-            'ip' => ['required', 'ip', 'max:255'],
-            'comment' => ['required', 'string', 'max:255'],
-            'email' => ['required', 'email', 'max:255', 'unique:users'],
-            'password' => ['required', 'string', Password::default()],
-        ]);
-        //Создаем пользователя только из валидных полей
-        if ($validator->passes() && $validated = $validator->validated()) {
-            $user = User::create([
-                'name' => $validated['name'],
-                'ip' => $validated['ip'],
-                'comment' => $validated['comment'],
-                'email' => $validated['email'],
-                'password' => $validated['password'],
-            ]);
-            return response($user);
-        }
-        return response($validator->messages());
+        return response($this->usersService->save($request));
     }
 
     /**
@@ -87,14 +52,7 @@ class UsersController extends Controller
      */
     public function show(string|int $id, Request $request): Response
     {
-        $request->merge(['id' => $id]);
-        $validator = Validator::make($request->all(), [
-            'id' => ['required', 'int', "exists:users,id,id,$request->id"],
-        ]);
-        if ($validator->passes() && $validated = $validator->validated()) {
-            return response(User::find($validated['id']));
-        }
-        return response($validator->messages());
+        return response($this->usersService->showOne($id, $request));
     }
 
     /**
@@ -108,37 +66,20 @@ class UsersController extends Controller
      */
     public function update(string|int $id, Request $request): Response
     {
-        $request->merge(['id' => $id]);
-        $validator = Validator::make($request->all(), [
-            'id' => ['required', 'int', "exists:users,id,id,$request->id"],
-            'name' => ['string', 'max:255'],
-            'ip' => ['string', 'max:255'],
-            'comment' => ['string', 'max:255'],
-            'password' => ['string', Password::default(), 'confirmed'],
-        ]);
-        //Обновляем пользователя только из валидных полей
-        if ($validator->passes() && $validated = $validator->validated()) {
-            $user = User::find($validated['id']);
-            $user->name = $validated['name'] ?? $user->name;
-            $user->ip = $validated['ip'] ?? $user->ip;
-            $user->comment = $validated['comment'] ?? $user->comment;
-            $user->password = $validated['password'] ?? $user->password;
-            $user->save();
-            return response($user);
-        }
-        return response($validator->messages());
+        return response($this->usersService->save($request, $id));
     }
 
     /**
      * Remove the specified resource from storage.
      *
-     * @param User $user
+     * @param string|int $id
+     * @param Request $request
      *
      * @return Response
+     * @throws ValidationException
      */
-    public function destroy(User $user): Response
+    public function destroy(string|int $id, Request $request): Response
     {
-        $user->delete();
-        return response();
+        return response($this->usersService->delete($id, $request));
     }
 }
