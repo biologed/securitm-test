3bda63ebc924534f325ba372358ef39863aa51d4
biologed <48013636+biologed@users.noreply.github.com>
2024-12-12 16:01:04 +0300

    fix routing

--- routes/api.php
+++ routes/api.php
@@ -6,9 +6,9 @@ use App\Http\Middleware\ForceJsonMiddleware;
 
 Route::group(['middleware' => [ForceJsonMiddleware::class]], static function () {
     Route::get('users', [UsersController::class, 'index'])->name('users.index');
+    Route::post('users', [UsersController::class, 'store'])->name('users.store');
     Route::get('users/{id}', [UsersController::class, 'show'])->name('users.show');
     Route::put('users/{id}', [UsersController::class, 'update'])->name('users.update');
-    Route::post('users/{id}', [UsersController::class, 'store'])->name('users.store');
     Route::delete('users/{id}', [UsersController::class, 'destroy'])->name('users.destroy');
 });
 
